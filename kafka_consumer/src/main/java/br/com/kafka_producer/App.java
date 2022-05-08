a50f3a89a12a0d36fcca0dfbe384569e713f6e79
lisdrl <lisandre.darioli@gmail.com>
2022-05-08 17:55:24 -0300

    Creation of new MVN project

---
+++ kafka_consumer/src/main/java/br/com/kafka_producer/App.java
@@ -0,0 +1,13 @@
+package br.com.kafka_consumer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
